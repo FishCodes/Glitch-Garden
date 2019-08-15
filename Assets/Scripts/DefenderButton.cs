6b5e2c4c72914520ea357a598eaac07e9586f39d
Stephen Fishbune <stfishbune@gmail.com>
2019-08-14 21:02:12 -0500

    Added Button Cost UI

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour {
 
@@ -10,6 +12,20 @@ public class DefenderButton : MonoBehaviour {
     private void Start()
     {
         darkColor = GetComponent<SpriteRenderer>().color;
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (!costText)
+        {
+            Debug.LogError(name + " has no cost text, add some!");
+        }
+        else
+        {
+            costText.text = defenderPrefab.GetStarCost().ToString();
+        }
     }
 
     private void OnMouseDown()
